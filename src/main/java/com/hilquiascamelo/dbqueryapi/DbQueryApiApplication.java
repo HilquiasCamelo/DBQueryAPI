a2e4d1c6f0f0d90245920bb4648888da718b90fb
HilquiasCamelo <hilquiaswpc10@outlook.com>
2023-03-28 11:56:21 -0300

    first commit

---
+++ src/main/java/com/hilquiascamelo/dbqueryapi/DbQueryApiApplication.java
@@ -0,0 +1,13 @@
+package com.hilquiascamelo.dbqueryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbQueryApiApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run ( DbQueryApiApplication.class , args );
+    }
+
+}
---
+++ src/test/java/com/hilquiascamelo/dbqueryapi/DbQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilquiascamelo.dbqueryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbQueryApiApplicationTests {
+
+    @Test
+    void contextLoads ( ) {
+    }
+
+}
